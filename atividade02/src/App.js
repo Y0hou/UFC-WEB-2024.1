9d79b395b661794692f45823f89b1954ef7af401
Nícolas Martins de Sousa <89361055+Y0hou@users.noreply.github.com>
2024-03-30 13:30:14 +0000

    new file:   atividade02/.gitignore 	new file:   atividade02/README.md 	new file:   atividade02/package-lock.json 	new file:   atividade02/package.json 	new file:   atividade02/public/index.html 	new file:   atividade02/src/App.js 	new file:   atividade02/src/components/01Context/FunctionContextA.jsx 	new file:   atividade02/src/components/01Context/FunctionContextB.jsx 	new file:   atividade02/src/components/01Context/FunctionContextC.jsx 	new file:   atividade02/src/components/01Context/FunctionContextD.jsx 	new file:   atividade02/src/components/01Context/MyColorContext.jsx 	new file:   atividade02/src/components/02Pokemon/ComponenteAvo.jsx 	new file:   atividade02/src/components/02Pokemon/ComponenteFilho.jsx 	new file:   atividade02/src/components/02Pokemon/ComponentePai.jsx 	new file:   atividade02/src/components/02Pokemon/ContextPokemon.jsx 	new file:   atividade02/src/components/03Pokemoncounter/ComponenteAvo.jsx 	new file:   atividade02/src/components/03Pokemoncounter/ComponenteFilho.jsx 	new file:   atividade02/src/components/03Pokemoncounter/ComponentePai.jsx 	new file:   atividade02/src/components/03Pokemoncounter/ContextPokemon.jsx 	new file:   atividade02/src/index.js

---
+++ atividade02/src/App.js
@@ -0,0 +1,17 @@
+import FunctionContextA from './components/01Context/FunctionContextA';
+import ComponenteAvo from './components/02Pokemon/ComponenteAvo';
+import ComponenteAvoCounter from './components/03Pokemoncounter/ComponenteAvo';
+
+function App() {
+  return (
+    <div className='Atividade 02'>
+      <FunctionContextA/>
+      <hr/>
+      <ComponenteAvo/>
+      <hr/>
+      <ComponenteAvoCounter/>
+    </div>
+  );
+}
+
+export default App;
